4a13ef418b3f96c5083252dad8c66aa4abd9dde6
afrotonder <o.rosado18@gmail.com>
2023-10-30 21:37:32 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'swiper-test',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
