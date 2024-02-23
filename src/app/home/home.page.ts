795e3ee9b85e4f12ae1201cd3a5dbd876e2963fe
nikitalpopov <nikita.popov@naturalcycles.com>
2024-02-23 16:58:45 +0100

    Vertical slider

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-home',
@@ -6,17 +7,13 @@ import { Component, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  // @ViewChild('gallerySwiper')   swiperRef: ElementRef | undefined;
+  weekdays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'].map(shortname => ({
+    shortname,
+  }))
 
-  testImages: Array<string>;
-
-  constructor() {
-
-    this.testImages = [
-      "../../assets/calaveras/1.png",
-      "../../assets/calaveras/2.png",
-      "../../assets/calaveras/3.png",
-    ]
-  }
-
+  testImages: Array<string> = Array.from({ length: 100 }, () => [
+    "https://dummyimage.com/600x400/ffa1ff/000000.jpg",
+    "https://dummyimage.com/600x500/a3ffd9/000000.jpg",
+    "https://dummyimage.com/600x600/849112/000000.jpg",
+  ]).flat()
 }
